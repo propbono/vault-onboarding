fde26eb51b41cc2bab2d7c23bfc063b973b34ff7
propbono <propbono@gmail.com>
2025-02-05 22:52:32 +0100

    :chore: scaffold basic nextjs app with tailwind and shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,9 @@
 import type { Config } from "tailwindcss";
+import animate from "tailwindcss-animate";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,11 +11,77 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
+        // Base colors
+        white: "var(--base-white)",
+        black: "var(--base-black)",
+        
+        // Grays
+        gray: {
+          50: "var(--gray-50)",
+          100: "var(--gray-100)",
+          200: "var(--gray-200)",
+          300: "var(--gray-300)",
+          800: "var(--gray-800)",
+          900: "var(--gray-900)",
+        },
+
+        // Brand colors
+        green: {
+          500: "var(--green-500)",
+          600: "var(--green-600)",
+          700: "var(--green-700)",
+          900: "var(--green-900)",
+        },
+
         background: "var(--background)",
         foreground: "var(--foreground)",
+        card: {
+          DEFAULT: "var(--card)",
+          foreground: "var(--card-foreground)"
+        },
+        popover: {
+          DEFAULT: "var(--popover)",
+          foreground: "var(--popover-foreground)"
+        },
+        primary: {
+          DEFAULT: "var(--primary)",
+          foreground: "var(--primary-foreground)"
+        },
+        secondary: {
+          DEFAULT: "var(--secondary)",
+          foreground: "var(--secondary-foreground)"
+        },
+        muted: {
+          DEFAULT: "var(--muted)",
+          foreground: "var(--muted-foreground)"
+        },
+        accent: {
+          DEFAULT: "var(--accent)",
+          foreground: "var(--accent-foreground)"
+        },
+        destructive: {
+          DEFAULT: "var(--destructive)",
+          foreground: "var(--destructive-foreground)"
+        },
+        border: "var(--border)",
+        input: "var(--input)",
+        ring: "var(--ring)",
+        chart: {
+          '1': "var(--chart-1)",
+          '2': "var(--chart-2)",
+          '3': "var(--chart-3)",
+          '4': "var(--chart-4)",
+          '5': "var(--chart-5)"
+        }
       },
-    },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)"
+      }
+    }
   },
-  plugins: [],
+  plugins: [animate],
 };
+
 export default config;
