b11a920d8af22d0658df9b1a7af1db6836739a28
propbono <propbono@gmail.com>
2025-02-07 21:37:15 +0100

    feat: update onboarding page to render onboarding form

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,7 +1,9 @@
-export default function Onboarding() {
+import { OnboardingForm } from '@/components/onboarding-form'
+
+export default function OnboardingPage() {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-gradient-light dark:bg-gradient-dark">
-      Onboarding
+    <main className="min-h-screen flex items-center justify-center bg-gradient-light dark:bg-gradient-dark px-6 md:px-8 mx-auto">
+      <OnboardingForm />
     </main>
   )
 }
